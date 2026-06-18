560a1ab32831bbaf82d7447c36e0eb1fcba16a78
Sales <gustavosales99999@gmail.com>
2026-06-18 03:11:51 -0300

    fix: remove .vercel/output from git to prevent vercel linux crash from windows build

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-BhFxZ4nz.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/Códigos/Tzolkin/Projetos/Global - Kevin/src/routes/__root.tsx", children: ["/", "/produto", "/checkout/return"], preloads: ["/assets/index-VuZlPWkg.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-VuZlPWkg.js" } }] }, "/produto": { filePath: "D:/Códigos/Tzolkin/Projetos/Global - Kevin/src/routes/produto.tsx", children: void 0, preloads: ["/assets/produto-njEZ-Q5x.js", "/assets/StripeEmbeddedCheckout-0ufgV4x_.js"] }, "/checkout/return": { filePath: "D:/Códigos/Tzolkin/Projetos/Global - Kevin/src/routes/checkout.return.tsx", children: void 0, preloads: ["/assets/checkout.return-CNJUZeYp.js", "/assets/StripeEmbeddedCheckout-0ufgV4x_.js"] } } });
-export {
-  tsrStartManifest
-};
